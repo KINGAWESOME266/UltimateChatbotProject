673d7e9761753acee74548777f08d9a13f85344e
KINGAWESOME266 <26085740+KINGAWESOME266@users.noreply.github.com>
2019-12-07 16:25:04 +0000

    Added the chatbot and working

--- UltimateChatbotProject/UltimateChatbotProject/Program.cs
+++ UltimateChatbotProject/UltimateChatbotProject/Program.cs
@@ -396,10 +396,143 @@ namespace UltimateChatbotProject
             }
             else
             {
+                Console.WriteLine("iChatbot is loading...");
 
+                Console.WriteLine("Let's Introduce Ourselves");
+                Console.WriteLine("My name is Siri. What's yours?");
+                string name = Console.ReadLine();
+
+                if (name == "Siri")
+                {
+                    Console.WriteLine("Hmmm");
+                }
+                else
+                {
+                    Console.WriteLine("Hello" + name + "nice to meet you");
+                }
+
+                Console.WriteLine("How old are you");
+                var age = Console.ReadLine();
+
+                if (Convert.ToInt32(age) == 14)
+                {
+                    Console.WriteLine("Me too!");
+                    Console.WriteLine("What year are you in ?");
+                    var year = Console.ReadLine();
+                    if (Convert.ToInt16(year) == 10)
+                    {
+                        Console.WriteLine("Same!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("I'm in year 10!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("I'm 14 years old");
+
+                    if (Convert.ToInt16(age) > Convert.ToInt16(14))
+                    {
+                        Console.WriteLine("I'm younger than you");
+                    }
+                    else
+                    {
+                        Console.WriteLine("I'm older than you");
+                    }
+                }
+                Console.WriteLine("What's your favorite TV Show");
+                string Show = Console.ReadLine();
+
+                if (Show == "Designated Survivor")
+                {
+                    Console.WriteLine("That's a great TV Show");
+                }
+
+                else
+                {
+                    Console.WriteLine("I haven't watched that yet");
+                    Console.WriteLine("Is it available on Netflix?");
+                    var yesbutton = Console.ReadKey();
+                    if (yesbutton.Key == ConsoleKey.Y)
+                    {
+                        Console.WriteLine("I'll check it out");
+                    }
+                    else
+                    {
+                        Console.WriteLine("I'll try to watch it somehow");
+                    }
+                }
+
+                Console.WriteLine("What's your favorite song?");
+                string song = Console.ReadLine();
+
+                if (song == "Secreto")
+                {
+                    Console.WriteLine("I love that song!");
+                }
+                else
+                {
+                    Console.WriteLine("Is it on Spotify?");
+                    var spotifybutton = Console.ReadKey();
+                    if (spotifybutton.Key == ConsoleKey.Y)
+                    {
+                        Console.WriteLine("I'll listen to it soon.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("I'll try to listen to it sometime.");
+                    }
+                }
+
+                Console.WriteLine(name + " do you have any pets?");
+                string pets = Console.ReadLine();
+
+                if (pets == "yes")
+                {
+                    Console.WriteLine("Do you have a cat or a dog " + name + " ?");
+                    string catdog = Console.ReadLine();
+
+                    if (catdog == "cat")
+                    {
+                        Console.WriteLine("I have a cat");
+                        Console.WriteLine("Is your cat big/small?");
+                        var catbutton = Console.ReadKey();
+                        if (catbutton.Key == ConsoleKey.B)
+                        {
+                            Console.WriteLine("I like big cats.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Small cats are cute!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("I don't like dogs");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("I understand");
+                }
+
+                Console.WriteLine("How did you enjoy this chatbot out of 1-10?");
+                var rating = Console.ReadLine();
+
+                if (Convert.ToInt16(rating) > 5)
+                {
+                    Console.WriteLine("Thank you for the nice rating!");
+                }
+                else
+                {
+                    Console.WriteLine("I hope to improve for next time.");
+                }
+                
             }
-            //Prevent the application from quitting automatically on completion.
-            Console.ReadLine();
+        
+        //Prevent the application from quitting automatically on completion.
+        Console.ReadLine();
         }
     }
 }
