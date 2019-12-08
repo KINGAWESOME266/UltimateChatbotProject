ab0bc6adff1a5cb4b25ae84f354b28098a76c692
KINGAWESOME266 <26085740+KINGAWESOME266@users.noreply.github.com>
2019-12-08 22:10:59 +0000

    Revert "BROKEN STUFF 2 - DO NOT USE"
    This reverts commit 31a01203be092c5959cd10617b4e41a0e276f2fa.

--- UltimateChatbotProject/UltimateChatbotProject/Program.cs
+++ UltimateChatbotProject/UltimateChatbotProject/Program.cs
@@ -5,7 +5,6 @@ namespace UltimateChatbotProject
 {
     class Program
     {
-        //The RPS Function
         public void RPS()
         {
             Random gen = new Random();
@@ -319,11 +318,8 @@ namespace UltimateChatbotProject
 
 
         }
-
-        //The chatbot function
         public void Chat()
         {
-            Program p = new Program();
             Console.WriteLine("\niChatbot is loading...");
             Thread.Sleep(5000);
             Console.WriteLine("Let's Introduce Ourselves");
@@ -456,7 +452,6 @@ namespace UltimateChatbotProject
             {
                 Console.WriteLine();
                 Console.WriteLine("Thank you for the nice rating!\n");
-                
             }
             else
             {
@@ -464,8 +459,6 @@ namespace UltimateChatbotProject
                 Console.WriteLine("I hope to improve for next time.\n");
             }
         }
-
-        //The function for the user to quit the program.
         public void Quit()
         {
             Console.WriteLine();
@@ -473,8 +466,6 @@ namespace UltimateChatbotProject
             Thread.Sleep(5000);
             Environment.Exit(0);
         }
-
-        //The function for the user to decide RPS/Quit.
         public void RPS2C()
         {
             Program p = new Program();
@@ -489,8 +480,6 @@ namespace UltimateChatbotProject
                 p.Quit();
             }
         }
-
-        //The function for the user to decide RPS/Quit.
         public void C2RPS()
         {
             Program p = new Program();
@@ -504,10 +493,9 @@ namespace UltimateChatbotProject
             {
                 p.Quit();
             }
-
-            static void Main(string[] args)
+        }
+        static void Main(string[] args)
         {
-            //The start of the program, with functions for each portion.
             Console.WriteLine("Welcome to Assistant. I'm your chatbot today.");
             Console.WriteLine("What do you want to do (R = RockPaperScissors, C = iChatbot, Q = Quit)");
             Program p = new Program();
@@ -517,10 +505,10 @@ namespace UltimateChatbotProject
                 p.RPS();
                 p.RPS2C();
             }
+            //The chatbot software below
             else if (button.Key == ConsoleKey.C)
             {
                 p.Chat();
-                p.C2RPS();
                 
             }
             else if (button.Key == ConsoleKey.Q)
